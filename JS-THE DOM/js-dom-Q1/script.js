4621d2715cd924848c84f0c16bfe9f54ef4e2a96
Kamal Manna <kamalmanna606@gmail.com>
2026-01-13 16:08:23 +0530

    Delete JS-THE DOM/js-dom-Q1/script.js

--- JS-THE DOM/js-dom-Q1/script.js
+++
@@ -1,9 +0,0 @@
-// Q.1 Create a button and a paragraph in html, when you click on the btn the text of paragraph will changed.
-
-var btn = document.querySelector('button');
-var p = document.querySelector('p');
-
-btn.addEventListener('click', function(){
-    p.textContent='Downloading started successfully!';
-    p.style.color="green";
-});
