93c659e6d711362ecb847371cfc3323204460c76
Kamal Manna <kamalmanna606@gmail.com>
2026-01-15 23:38:55 +0530

    Add files via upload

---
+++ JS-THE DOM/js-dom-Q5/script.js
@@ -0,0 +1,19 @@
+//5. Build a countdown timer that starts when a button is clicked and updates the display in real-time.
+
+var start = document.querySelector("#start");
+var h3 = document.querySelector("h3");
+var stop = document.querySelector("#stop");
+
+var int;
+
+start.addEventListener('click', function(){
+       var count = 0;
+       int = setInterval(function(){
+        h3.textContent=count;
+        count++;
+       },1000);       
+});
+
+stop.addEventListener('click',function(){
+        clearInterval(int);
+});
