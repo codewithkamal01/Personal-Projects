fabb1a615924711dd2383ab3a97228e13ee73835
Kamal Manna <kamalmanna606@gmail.com>
2026-01-16 11:17:23 +0530

    Add files via upload

---
+++ JS-THE DOM/js-dom-Q7/script.js
@@ -0,0 +1,13 @@
+//7.Display a progress bar that updates in real-time, showing the progress of a task, download, or form submission.
+
+var prg = document.querySelector("#progress");
+var h3 = document.querySelector("h3")
+var count = 0;
+var int = setInterval(function () {
+  if (count === 100) {
+    h3.style.opacity=1;
+    clearInterval(int);
+  }
+  count++;
+  prg.style.width = count + "%";
+}, 50);
